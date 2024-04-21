e35dd811cdadde36f87a0d54e0662a15dab85834
Vladimir Guevara <go17005@ues.edu.sv>
2024-04-21 12:58:32 -0600

    first commit

---
+++ GO17005/GO17005/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GO17005
+//
+//  Created by Vladimir Guevara on 21/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GO17005/GO17005/GO17005App.swift
@@ -0,0 +1,17 @@
+//
+//  GO17005App.swift
+//  GO17005
+//
+//  Created by Vladimir Guevara on 21/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GO17005App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
